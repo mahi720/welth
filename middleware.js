e9e698c1c62a0051fe494845943571e8812608a0
Sonu Kumar <121798722+mahi720@users.noreply.github.com>
2025-02-11 19:42:02 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -6,50 +6,50 @@ const isProtectedRoute = createRouteMatcher([
   "/transaction(.*)",
 ]);
 
-// Create Arcjet middleware
-const aj = arcjet({
-  key: process.env.ARCJET_KEY,
-  // characteristics: ["userId"], // Track based on Clerk userId
-  rules: [
-    // Shield protection for content and security
-    shield({
-      mode: "LIVE",
-    }),
-    detectBot({
-      mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
-      allow: [
-        "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
-        "GO_HTTP", // For Inngest
-        // See the full list at https://arcjet.com/bot-list
-      ],
-    }),
-  ],
-});
+// // Create Arcjet middleware
+// const aj = arcjet({
+//   key: process.env.ARCJET_KEY,
+//   // characteristics: ["userId"], // Track based on Clerk userId
+//   rules: [
+//     // Shield protection for content and security
+//     shield({
+//       mode: "LIVE",
+//     }),
+//     detectBot({
+//       mode: "LIVE", // will block requests. Use "DRY_RUN" to log only
+//       allow: [
+//         "CATEGORY:SEARCH_ENGINE", // Google, Bing, etc
+//         "GO_HTTP", // For Inngest
+//         // See the full list at https://arcjet.com/bot-list
+//       ],
+//     }),
+//   ],
+// });
 
-// Create base Clerk middleware
-const clerk = clerkMiddleware(async (auth, req) => {
-  const { userId } = await auth();
-
-  if (!userId && isProtectedRoute(req)) {
-    const { redirectToSignIn } = await auth();
-    return redirectToSignIn();
-  }
-
-  return NextResponse.next();
-});
-
-// Chain middlewares - ArcJet runs first, then Clerk
-export default createMiddleware(aj, clerk);
-
-// export default clerkMiddleware(async (auth, req) => {
+// // Create base Clerk middleware
+// const clerk = clerkMiddleware(async (auth, req) => {
 //   const { userId } = await auth();
 
 //   if (!userId && isProtectedRoute(req)) {
 //     const { redirectToSignIn } = await auth();
-
 //     return redirectToSignIn();
 //   }
+
+//   return NextResponse.next();
 // });
+
+// // Chain middlewares - ArcJet runs first, then Clerk
+// export default createMiddleware(aj, clerk);
+
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+
+    return redirectToSignIn();
+  }
+});
 
 export const config = {
   matcher: [
